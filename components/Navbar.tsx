c2e521fc26d410fbf3ebe70799854e51471f04d4
Mateus Santos <mateus@gmail.com>
2026-07-27 12:31:08 -0300

    Remove endereco do topo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,14 +3,6 @@ export default function Navbar() {
     <nav className="bg-black text-white p-4 shadow">
 
       <p className="text-center text-sm">
-        📍 Rua Vinte e Cinco de Dezembro, Nº 8
-      </p>
-
-      <p className="text-center text-sm">
-        Bairro Retiro Saudoso - Cariacica/ES
-      </p>
-
-      <p className="text-center text-sm mt-1">
         Seu estilo, nosso cuidado
       </p>
 
